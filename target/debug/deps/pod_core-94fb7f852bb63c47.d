af54edce09a1e90223cac0b1a8c584eda37c99b9
developeruche <developeruche@gmail.com>
2025-08-27 14:31:23 +0100

    clean

--- target/debug/deps/pod_core-94fb7f852bb63c47.d
+++
@@ -1,5 +0,0 @@
-/Users/gregg/Documents/projects/PROJECTS/pod-network/target/debug/deps/pod_core-94fb7f852bb63c47.d: crates/pod-core/src/lib.rs
-
-/Users/gregg/Documents/projects/PROJECTS/pod-network/target/debug/deps/libpod_core-94fb7f852bb63c47.rmeta: crates/pod-core/src/lib.rs
-
-crates/pod-core/src/lib.rs:
--- target/debug/deps/pod_core-999ff46974dfc789.d
+++
@@ -1,5 +0,0 @@
-/Users/gregg/Documents/projects/PROJECTS/pod-network/target/debug/deps/pod_core-999ff46974dfc789.d: crates/pod-core/src/lib.rs
-
-/Users/gregg/Documents/projects/PROJECTS/pod-network/target/debug/deps/libpod_core-999ff46974dfc789.rmeta: crates/pod-core/src/lib.rs
-
-crates/pod-core/src/lib.rs:
